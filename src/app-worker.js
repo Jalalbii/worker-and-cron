eb03bf6365a75ad910e056e453762809c0d0bfb1
Jalalbii <8451031656safa@gmail.com>
2025-07-27 17:47:55 +0530

    winston logging added

--- src/app-worker.js
+++ src/app-worker.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const {Worker} = require("worker_threads")
+const logger = require("./logger")
+const path = require("path")
 
 const app = express()
 
 const runWorker = (data) => {
     return new Promise((resolve, reject) => {
-        const worker = new Worker("./worker.js", {
+        const worker = new Worker(path.join(__dirname, 'worker.js'), {
             workerData: data
         })
         worker.on('message', (data) => {
@@ -23,10 +25,14 @@ const runWorker = (data) => {
 app.get("/heavy", async(req, res) => {
     try{
         const result = await runWorker(5)
+        console.log("RESULT")
+        logger.info('Heavy task')
         res.json({
             data: result
         })
     }catch(err){
+        console.log("ERROR", err)
+        logger.error('Error')
         res.json({
             message: "Error"
         })
